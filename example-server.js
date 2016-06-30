dd18663b09c303a24caff682497a5817074f33ca
philcal <philip.callender@tooltwist.com>
2016-06-30 14:25:06 +0800

    Bind example server to 0.0.0.0
    - on OSX this causes binding to the external IP address rather than to
    localhost, which is only accessible internally.

--- example-server.js
+++ example-server.js
@@ -1,5 +1,11 @@
+/*
+ *	A super small web server to serve up content from the current directory.
+ */
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
+
+// Binding to 0.0.0.0 causes OSX to bind to the external
+// IP address of the machine rather than to localhost.
+connect().use(serveStatic(__dirname)).listen(8080, '0.0.0.0', function(){
     console.log('Server running on 8080...');
 });
